a91cdc94ad8b280a228e0451ca00199df8488d7f
Irina Barreto <abraeva.98@icloud.com>
2020-06-25 15:16:33 -0400

    created tables

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require("morgan");
 const router = require("./routes/routes");
-
+const { db } = require('./models');
+const models = require('./models');
 const app = express();
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public/stylesheets"));
 app.use(express.urlencoded({ extended: false }));
 
 
+
+
 app.use('/', router)
+
+const init = async() => {
+  try {
+await models.db.sync()
 
 
  const PORT = 4001;
@@ -16,3 +23,7 @@ app.use('/', router)
  app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`);
   });
+} catch(err) {console.log(err)}
+}
+
+init()
